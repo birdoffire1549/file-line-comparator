064fc957eedfaa3439af6762cc499e5939d0f5cf
Scott Griffis <scottgriffis@solutionary.com>
2016-10-10 11:29:51 -0500

    tool is more complete though not yet tested

--- src/main/java/com/firebirdcss/tools/file_line_comparator/ApplicationMain.java
+++ src/main/java/com/firebirdcss/tools/file_line_comparator/ApplicationMain.java
@@ -4,6 +4,12 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Application's main point of entry.
+ * 
+ * @author Scott Griffis
+ *
+ */
 public class ApplicationMain {
 	private static File file1 = null;
 	private static File file2 = null;
@@ -37,6 +43,11 @@ public class ApplicationMain {
 			} catch (InterruptedException ok) {}
 		}
 		
+		if (processor1.isReadProblems() || processor2.isReadProblems()) {
+			System.out.println("ERROR: There were problems processing at least one of the files; Please check to ensure the files are valid!");
+			System.exit(1);
+		}
+		
 		List<String> dumpList = new ArrayList<>();
 		for (String item : lines1) {
 			if (lines2.remove(item)) {
@@ -49,21 +60,64 @@ public class ApplicationMain {
 		displayResults(lines1, lines2);
 	}
 	
+	/**
+	 * Displays the results.
+	 * 
+	 * @param lines1 - Final list of non-matching lines from file 1 as {@link List} of {@link String}s
+	 * @param lines2 - Final list of non-matching lines from file 2 as {@link List} of {@link String}s
+	 */
 	private static void displayResults(List<String> lines1, List<String> lines2) {
 		if (lines1.size() == 0 && lines2.size() == 0) {
-			// TODO: Yea! the files had the same contents...
+			System.out.println("The two files are the same.");
 		} else {
 			if (lines1.size() > 0) {
-				
+				System.out.println("File #1 contains '" + lines1.size() + "' lines that were not in file #2.");
+			}
+			
+			if (lines2.size() > 0) {
+				System.out.println("File #2 contains '" + lines2.size() + "' lines that were not in file #1.");
 			}
 		}
 	}
 	
+	/**
+	 * Handles processing the arguments passed into the file.
+	 * 
+	 * @param args - The arguments as a {@link String} array
+	 */
 	private static void handleArgs(String[] args) {
-		
+		for (String arg : args) {
+			switch (arg) {
+				case "-h":
+				case "--help":
+					displayUsage();
+					break;
+				default:
+					if (file1 == null) {
+						file1 = new File(arg);
+						if (!(file1.isFile() && file1.canRead())) {
+							System.out.println("ERROR: The supplied file #1 of '" + file1.getName() + "' is either not a file or not readable!");
+							System.exit(1);
+						}
+					} else if (file2 == null) {
+						file2 = new File(arg);
+						if (!(file2.isFile() && file2.canRead())) {
+							System.out.println("ERROR: The supplied file #2 of '" + file2.getName() + "' is either not a file or not readable!");
+							System.exit(1);
+						}
+					} else {
+						displayUsage();
+						System.exit(1);
+					}
+					break;
+			}
+		}
 	}
 	
+	/**
+	 * Displays the program usage syntax for the user.
+	 */
 	private static void displayUsage() {
-		
+		System.out.println("usage: file-line-comparator <file 1> <file 2>\n");
 	}
 }
--- src/main/java/com/firebirdcss/tools/file_line_comparator/FileProcessor.java
+++ src/main/java/com/firebirdcss/tools/file_line_comparator/FileProcessor.java
@@ -6,6 +6,10 @@ import java.io.FileReader;
 import java.util.List;
 
 /**
+ * This class handles the processing of a file which is assigned to it.
+ * <p>
+ * As part of the processing this class breaks a file down into a {@link List} of lines that are trimmed.
+ * Empty lines are not added to the file List.
  * 
  * @author Scott Griffis
  *
@@ -16,7 +20,7 @@ public class FileProcessor extends Thread {
 	private volatile List<String> lines;
 	
 	/**
-	 * 
+	 * CONSTRUCTOR: Initialization of variables happens here.
 	 * @param file
 	 * @param listForLines
 	 */
@@ -26,8 +30,10 @@ public class FileProcessor extends Thread {
 	}
 	
 	/**
+	 * This method can be used post-processing to see if there was a read error generated
+	 * during the file read process.
 	 * 
-	 * @return
+	 * @return Returns true if there was a error while reading the file, otherwise false as {@link boolean}
 	 */
 	public boolean isReadProblems() {
 		
