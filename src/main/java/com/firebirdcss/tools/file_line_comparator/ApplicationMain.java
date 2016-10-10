04b13f18d697b5536bc3fc564f0f53a4eb5e5ce4
Scott Griffis <scottgriffis@solutionary.com>
2016-10-10 12:14:41 -0500

    Added headers

--- src/main/java/com/firebirdcss/tools/file_line_comparator/ApplicationMain.java
+++ src/main/java/com/firebirdcss/tools/file_line_comparator/ApplicationMain.java
@@ -1,3 +1,21 @@
+/* 
+ * file-line-comparator - Compares the lines of two files to see if they are equivalent regardless of ordering.
+ * Copyright (C) 2016,  Scott Griffis
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.firebirdcss.tools.file_line_comparator;
 
 import java.io.File;
